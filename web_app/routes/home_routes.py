4c0274e91a5c04f3af76e9b26b4b68675ea9d106
Lesley Lam <lyl13@georgetown.edu>
2022-12-07 15:27:25 -0500

    Basic home page created

---
+++ web_app/routes/home_routes.py
@@ -0,0 +1,13 @@
+# this is the "web_app/routes/home_routes.py" file...
+
+from flask import Blueprint, request, render_template
+
+home_routes = Blueprint("home_routes", __name__)
+
+
+@home_routes.route("/")
+@home_routes.route("/home")
+def index():
+    print("HOME...")
+    return "Welcome Home"
+    #return render_template("home.html")
