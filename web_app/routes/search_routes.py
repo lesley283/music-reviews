2964454fe4a4acebb4f3b0a7b436bbe71dcc833d
lesley283 <97481618+lesley283@users.noreply.github.com>
2022-12-09 15:34:16 -0500

    Clean up (#12)
    * Refactor code
    
    * Refactor past_reviews_route
    
    * Add web app testing
    
    * Update testing
    
    * Clean up code
    
    * Add comments, clean up code
    
    * Clean up
    
    * Update web_app_test.py

--- web_app/routes/search_routes.py
+++ web_app/routes/search_routes.py
@@ -26,20 +26,20 @@ def search_validation():
 
         all_reviews = load_pickle_data() # load past reviews from pickle file
 
-        avg_rating, match_reviews, match_user, match_song = load_matching_reviews(name, review_level, all_reviews)
+        avg_rating, match_reviews, match_user, match_song, match_artist, match_album = load_matching_reviews(name, review_level, all_reviews)
 
         # if list is empty, there is nothing that matches the search criteria
         if len(avg_rating) == 0:
             flash("There seems to be no reviews for that particular " + review_level + ". Please make sure you have entered the name correctly or leave a review yourself!", "danger")
             return redirect("/search-review")
         else:
-            rating_output, reviews, ratings, users, song = reviews_output(avg_rating, match_reviews, match_user, match_song) # return the correct output, including 5 most recent reviews and average rating
+            rating_output, reviews, ratings, users, song, artist, album = reviews_output(avg_rating, match_reviews, match_user, match_song, match_artist, match_album) # return the correct output, including 5 most recent reviews and average rating
 
             return render_template("search_output.html",
                                     name=name,
                                     review_level=review_level,
                                     rating_output=rating_output,
-                                    review_list=zip(reviews, ratings, users, song)
+                                    review_list=zip(reviews, ratings, users, song, artist, album)
                                 )
 
     except Exception as err:
